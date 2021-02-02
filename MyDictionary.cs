90c37fe537bd7318d400e03eee74c97d671e67fe
ayseozates <69298065+ayseozates@users.noreply.github.com>
2021-02-02 11:48:31 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+  public  class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keyArray;
+        TKey[] _tempKey;
+        TValue[] _valueArray;
+        TValue[] _tempValue;
+
+        public MyDictionary()
+        {
+            _keyArray = new TKey[0];
+            _valueArray = new TValue[0];
+
+        }
+        public void Add(TKey key,TValue value)
+        {
+            _tempKey = _keyArray;
+            _keyArray = new TKey[_keyArray.Length + 1];
+
+            _tempValue = _valueArray;
+            _valueArray = new TValue[_valueArray.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _keyArray[i] = _tempKey[i];
+                _valueArray[i] = _tempValue[i];
+
+            }
+            _keyArray[_keyArray.Length - 1] = key;
+            _valueArray[_valueArray.Length - 1] = value;
+
+            Console.WriteLine("Plaka: " + key + " " + "Şehir: " + value);
+        }
+    
+        public TKey[] Keys
+        {
+            get { return _keyArray; }
+        }
+        public TValue[] Values
+        {
+            get { return _valueArray; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string>Plaka = new MyDictionary<int, string>();
+            Plaka.Add(23, "Elazığ");
+            Plaka.Add(12, "Bingöl");
+            //foreach (KeyValuePair<string, string> kvp in myDictionary)
+            //{
+            //    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            //}
+        }
+    }
+}
